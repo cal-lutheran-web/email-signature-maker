eacb7c6ad1d55b07b7aa65ceecbb908ad49b528a
Scott Smith <scottsmith24@gmail.com>
2019-09-20 16:50:18 -0700

    get dimensions from image

--- src/components/Sig.vue
+++ src/components/Sig.vue
@@ -1,27 +1,80 @@
 <template>
-<div class="sig">
-	<p style="padding: 0; font-size: 12px; line-height: 15px; font-family: Verdana; color: #3b2360; margin: 0 0 15px 0;">
-			<b>{{ formData.name }}</b><br>
-			<span v-if="formData.pronouns"><strong>Pronouns:</strong> {{ formData.pronouns_display }}<br /></span>
-			<i>{{ formData.title }}<br>
-			<span v-if="formData.department">{{ formData.department }}</span></i>
+	<div class="sig">
+		<p style="padding: 0; font-size: 12px; line-height: 15px; font-family: Verdana; color: #3b2360; margin: 0 0 15px 0;">
+			<b>{{ formData.name ? formData.name : "First Last" }}</b>
+			<br />
+			<span v-if="formData.pronouns">
+				<strong>Pronouns:</strong>
+				{{ formData.pronouns_display }}
+				<br />
+			</span>
+			<i>
+				{{ formData.title ? formData.title : "Title" }}
+				<br />
+				<span v-if="formData.department">{{
+					formData.department
+				}}</span>
+			</i>
 		</p>
-		<img src="https://www.callutheran.edu/offices/marketing/brand/images/CLU-default-signature.png" alt="California Lutheran University" width="250" height="24">
-		<p style="padding: 0; font-size: 10px; line-height: 14px; font-family: Verdana; color: #777777; margin: 8px 0 0 0;">
-			60 West Olsen Road #{{ formData.mail_code }} &nbsp;|&nbsp; Thousand Oaks, CA 91360<br>
-			<span v-if="formData.phone">Phone: (805) 493-{{ formData.phone }} </span><span v-if="formData.fax">&nbsp;|&nbsp; Fax: (805) 493-{{ formData.fax }}</span><br>
+		<img
+			:src="formData.logos[formData.logo]"
+			alt="California Lutheran University"
+			width="250"
+			:height="this.logo_h ? this.logo_h : 24"
+		/>
+		<p
+			style="padding: 0; font-size: 10px; line-height: 14px; font-family: Verdana; color: #777777; margin: 8px 0 0 0;"
+		>
+			60 West Olsen Road #{{
+				formData.mail_code ? formData.mail_code : "XXXX"
+			}}
+			&nbsp;|&nbsp; Thousand Oaks, CA 91360
+			<br />
+			<span v-if="formData.phone"
+				>Phone: (805) 493-{{
+					formData.phone ? formData.phone : "XXXX"
+				}}</span
+			>
+			<span v-if="formData.fax"
+				>&nbsp;|&nbsp; Fax: (805) 493-{{
+					formData.fax ? formData.fax : "XXXX"
+				}}</span
+			>
+			<br />
 			<a href="https://www.callutheran.edu">CalLutheran.edu</a>
-	</p>
+		</p>
 	</div>
 </template>
 
 <script>
-export default {
-	name: 'Sig',
-	props: ['formData']
-}
+	import Vue from "vue";
+
+	export default {
+		name: "Sig",
+		props: ["formData"],
+		data() {
+			return {
+				logo_w: '',
+				logo_h: ''
+			}
+		},
+		watch: {
+			'formData.logo': function(data){
+				var $this = this;
+				var img = new Image();
+				img.src = this.formData.logos[this.formData.logo];
+				img.onload = function() {
+					console.log(this.width,this.height);
+					$this.logo_w = this.width;
+					$this.logo_h = this.height;
+				}
+			}
+		}
+	};
 </script>
 
 <style>
-
+	.sig {
+		position: fixed;
+	}
 </style>
