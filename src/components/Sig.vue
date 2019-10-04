5926067a9a26a0f6efefbad9a38f202ee8a0ea40
Scott Smith <scottsmith24@gmail.com>
2019-10-04 14:27:58 -0700

    copy to clipboard support

--- src/components/Sig.vue
+++ src/components/Sig.vue
@@ -1,78 +1,84 @@
 <template>
-	<div id="signature-wrapper" class="sig" @click="toClipboard">
+	<div class="signature-wrapper">
+		<div id="signature-content" class="sig" @click="toClipboard">
 
-		<p style="padding: 0; font-size: 12px; line-height: 15px; font-family: Verdana; color: #3b2360; margin: 0 0 15px 0;">
-			<b>{{ formData.name ? formData.name : "First Last" }}</b>
-			<br />
-			<i>
-				{{ formData.title ? formData.title : "Title" }}
-				<template v-if="formData.department"><br />{{ formData.department }}</template>
-				<template v-if="formData.pronouns"><br />Pronouns: {{ formData.pronouns_display }}</template>
-			</i>
+			<p style="padding: 0; font-size: 12px; line-height: 15px; font-family: Verdana; color: #3b2360; margin: 0 0 15px 0;">
+				<b>{{ formData.name ? formData.name : "First Last" }}</b>
+				<br />
+				<i>
+					{{ formData.title ? formData.title : "Title" }}
+					<template v-if="formData.department"><br />{{ formData.department }}</template>
+					<template v-if="formData.pronouns"><br />Pronouns: {{ formData.pronouns_display }}</template>
+				</i>
+				
+			</p>
+
+			<img
+				:src="formData.logos[formData.logo]"
+				alt="California Lutheran University"
+				width="250"
+				:height="this.logo_h ? this.logo_h : 24"
+				v-if="formData.logo !== 'plts'"
+			/>
 			
-		</p>
+			<img
+				:src="formData.logos.plts"
+				alt="Pacific Lutheran Theological Seminary"
+				width="250"
+				:height="this.logo_h ? this.logo_h : 24"
+				v-if="formData.logo == 'plts'"
+			/>
 
-		<img
-			:src="formData.logos[formData.logo]"
-			alt="California Lutheran University"
-			width="250"
-			:height="this.logo_h ? this.logo_h : 24"
-			v-if="formData.logo !== 'plts'"
-		/>
-		
-		<img
-			:src="formData.logos.plts"
-			alt="Pacific Lutheran Theological Seminary"
-			width="250"
-			:height="this.logo_h ? this.logo_h : 24"
-			v-if="formData.logo == 'plts'"
-		/>
+			<p style="padding: 0; font-size: 10px; line-height: 14px; font-family: Verdana; color: #777777; margin: 8px 0 0 0;" >
+				<template v-if="formData.logo == 'plts'">
+					2000 Center Street, Suite 200 Berkeley, CA 94704
+				</template>
 
-		<p style="padding: 0; font-size: 10px; line-height: 14px; font-family: Verdana; color: #777777; margin: 8px 0 0 0;" >
-			<template v-if="formData.logo == 'plts'">
-				2000 Center Street, Suite 200 Berkeley, CA 94704
-			</template>
+				<template v-else>
+					60 West Olsen Road <template v-if="formData.mail_code">#{{ formData.mail_code }}</template>&nbsp;|&nbsp;Thousand Oaks, CA 91360
+				</template>
 
-			<template v-else>
-				60 West Olsen Road <template v-if="formData.mail_code">#{{ formData.mail_code }}</template>&nbsp;|&nbsp;Thousand Oaks, CA 91360
-			</template>
+				<br v-if="formData.phone || formData.fax" />
+					<template v-if="formData.phone">Phone: (805) 493-{{ formData.phone ? formData.phone : "XXXX" }}</template>
+					<template v-if="formData.fax">&nbsp;|&nbsp; Fax: (805) 493-{{ formData.fax ? formData.fax : "XXXX" }}</template>
+				<br />
 
-			<br v-if="formData.phone || formData.fax" />
-				<template v-if="formData.phone">Phone: (805) 493-{{ formData.phone ? formData.phone : "XXXX" }}</template>
-				<template v-if="formData.fax">&nbsp;|&nbsp; Fax: (805) 493-{{ formData.fax ? formData.fax : "XXXX" }}</template>
-			<br />
+				<a :href="'https://www.callutheran.edu/' + formData.website" v-if="formData.logo !=='plts'">CalLutheran.edu{{ formData.website ? '/'+formData.website : '' }}</a>
+				<a :href="'https://www.plts.edu/' + formData.website" v-if="formData.logo == 'plts'">PLTS.edu{{ formData.website ? '/'+formData.website : '' }}</a>
+			</p>
 
-			<a :href="'https://www.callutheran.edu/' + formData.website" v-if="formData.logo !=='plts'">CalLutheran.edu{{ formData.website ? '/'+formData.website : '' }}</a>
-			<a :href="'https://www.plts.edu/' + formData.website" v-if="formData.logo == 'plts'">PLTS.edu{{ formData.website ? '/'+formData.website : '' }}</a>
-		</p>
+			<p v-if="formData.social_media_display" style="margin-top: 1em">
+				<a :href="formData.social_media.facebook" v-if="formData.social_media.facebook">
+					<img src="https://www.callutheran.edu/offices/marketing/brand/email/images/facebook.png" width="24" height="24" />
+				</a>
+				<a :href="formData.social_media.instagram" v-if="formData.social_media.instagram">
+					<img src="https://www.callutheran.edu/offices/marketing/brand/email/images/instagram.png" width="24" height="24" />
+				</a>
+				<a :href="formData.social_media.twitter" v-if="formData.social_media.twitter">
+					<img src="https://www.callutheran.edu/offices/marketing/brand/email/images/twitter.png" width="24" height="24" />
+				</a>
+				<a :href="formData.social_media.youtube" v-if="formData.social_media.youtube">
+					<img src="https://www.callutheran.edu/offices/marketing/brand/email/images/youtube.png" width="24" height="24" />
+				</a>
+			</p>
+		</div>
 
-		<p v-if="formData.social_media_display" style="margin-top: 1em">
-			<a :href="formData.social_media.facebook" v-if="formData.social_media.facebook">
-				<img src="https://www.callutheran.edu/offices/marketing/brand/email/images/facebook.png" width="24" height="24" />
-			</a>
-			<a :href="formData.social_media.instagram" v-if="formData.social_media.instagram">
-				<img src="https://www.callutheran.edu/offices/marketing/brand/email/images/instagram.png" width="24" height="24" />
-			</a>
-			<a :href="formData.social_media.twitter" v-if="formData.social_media.twitter">
-				<img src="https://www.callutheran.edu/offices/marketing/brand/email/images/twitter.png" width="24" height="24" />
-			</a>
-			<a :href="formData.social_media.youtube" v-if="formData.social_media.youtube">
-				<img src="https://www.callutheran.edu/offices/marketing/brand/email/images/youtube.png" width="24" height="24" />
-			</a>
-		</p>
+		<button @click="toClipboard" class="btn btn-small green">Copy to Clipboard</button>
+
+		<p v-if="this.confirmCopy">Copied to Clipboard</p>
 	</div>
 </template>
 
 <style>
 	.sig {
 		padding: 2rem;
+		margin-bottom: 1rem;
 		border: 1px solid #bfc5c9;
-		display: inline-block;
 		position: relative;
 	}
 
 	.sig::before {
-		content: "Select the Contents and Copy";
+		content: "Select the Contents";
 		display: inline-block;
 		background: #bfc5c9;
 		color: white;
@@ -82,6 +88,10 @@
 		position: absolute;
 		top: calc(-1em + -0.5rem + -1px);
 		left: -1px;
+	}
+
+	.btn {
+		margin-bottom: 1rem;
 	}
 </style>
 
@@ -94,7 +104,8 @@
 		data() {
 			return {
 				logo_w: "",
-				logo_h: ""
+				logo_h: "",
+				confirmCopy: false
 			};
 		},
 		watch: {
@@ -109,7 +120,21 @@
 			}
 		},
 		methods: {
-			toClipboard() {}
+			toClipboard() {
+				var sigArea = document.querySelector("#signature-content")
+					.innerHTML;
+
+				function listener(e) {
+					e.clipboardData.setData("text/html", sigArea);
+					e.clipboardData.setData("text/plain", sigArea);
+					e.preventDefault();
+				}
+				document.addEventListener("copy", listener);
+				document.execCommand("copy");
+				document.removeEventListener("copy", listener);
+
+				this.confirmCopy = true;
+			}
 		}
 	};
 </script>
