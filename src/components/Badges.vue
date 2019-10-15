5429e769b7c3c4e858cb09db8c4d84495d18cc6e
Scott Smith <scottsmith24@gmail.com>
2019-10-15 15:16:02 -0700

    badge select color

--- src/components/Badges.vue
+++ src/components/Badges.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="form-item form-item-inline form-item-img">
-		<label :for="this.$vnode.key">
+		<label :for="this.$vnode.key" :class="{ checked : badge.display }">
 			<span class="sr-only">{{ badge.name }}</span>
 			<img :src="badge.img" width="60px" height="auto" />
 		</label>
@@ -19,5 +19,20 @@ export default {
 <style>
 	.form-item-img label {
 		cursor: pointer;
+		padding: .25rem;
 	}
+
+	.form-item-img label img {
+		opacity: 0.75;
+	}
+
+	.form-item-img label.checked {
+		background-color: #00854f;
+	}
+
+	.form-item-img label.checked img {
+		opacity: 1;
+	}
+
+	.form-item-img input { display: none; }
 </style>
